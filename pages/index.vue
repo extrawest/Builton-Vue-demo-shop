5fd53a3f5b2a3c63e1ee00786088f619c228ebdf
Eugen Hovorun <yevhen.hovorun@extrawest.com>
2019-10-24 10:19:52 +0300

    add home-wrapper

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,98 @@
 <template>
-  <div>
+  <div class="home-wrapper">
+    <div class="background-left-fragment"></div>
+
+    <div class="left-category-wrapper">
+      <ImageCategory category="adidas" />
+    </div>
+    <div class="inner-wrapper">
+      <ImageCategory category="nike" />
+      <ImageCategory category="puma" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator'
+    import ImageCategory from '~/components/ImageCategory.vue'
 
-    @Component({})
-    export default class HomePage extends Vue {}
+    @Component({
+        components: {
+            ImageCategory
+        }
+    })
+    export default class HomePage extends Vue {
+    }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  @import '../assets/variables';
+
+  .home-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    min-height: calc(100vh - 48px);
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    position: relative;
+    overflow: hidden;
+
+    &:before {
+      position: absolute;
+      content: '';
+      width: 200%;
+      height: 100%;
+      background-color: @TERTIARY_BACKGROUND;
+      left: 50%;
+      transform: translateX(10%) skew(-50deg);
+    }
+
+    & .background-left-fragment {
+      &:before {
+        position: absolute;
+        content: '';
+        width: 200%;
+        height: 100%;
+        background-color: @ORANGE_BACKGROUND;
+        right: 100%;
+        bottom: -20%;
+        transform: translateX(5%) skew(70deg);
+      }
+    }
+
+    & > div:not(:last-child) {
+      padding: 12px 24px;
+    }
+
+    & .inner-wrapper {
+      & > div {
+        padding: 12px 48px;
+      }
+    }
+
+    .second-wrapper {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+
+  @media all and (max-width: 720px) {
+    .home-wrapper {
+      flex-direction: column;
+
+      & > div:not(:last-child) {
+        padding: 12px 0;
+      }
+
+      & .inner-wrapper {
+        & > div {
+          padding: 12px 0;
+        }
+      }
+    }
+  }
+</style>
